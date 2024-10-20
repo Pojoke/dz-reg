21653251163623074c50b830e99a7f410dceb3bd
Pojoke <pavlykdmutro@gmail.com>
2024-10-20 12:21:29 +0300

    dz2

--- 3_2.php
+++ 3_2.php
@@ -7,7 +7,7 @@ $text = '<p>Права пользователей:</p>
   <li>Editor</li>
   <li>Subscriber</li>
 </ul>';
-$pat = '/<li>(.*?)<\/li>/';
+$pat = '%<li>(/w+)</li>%';
 $repl = '<li><a href="http://www.php.kh.ua/script.php?role=$1">$1</a></li>';
 $res = preg_replace($pat, $repl, $text);
 print_r($res);
